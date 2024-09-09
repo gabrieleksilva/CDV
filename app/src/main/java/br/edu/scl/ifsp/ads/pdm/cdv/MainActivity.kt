d90071c238d59d59dece2d8f92a6790237a578f7
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-09-09 19:57:21 -0300

    salvando o valor de um campo dinamico, que é criado em tempo de execução

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/cdv/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/cdv/MainActivity.kt
@@ -3,6 +3,10 @@ package br.edu.scl.ifsp.ads.pdm.cdv
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.ads.pdm.cdv.databinding.ActivityMainBinding
 
@@ -11,9 +15,24 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    companion object{
+        const val VALOR_TELEFONE_UM = "VALOR_TELEFONE_UM"
+        const val VALOR_TELEFONE_DOIS = "VALOR_TELEFONE_DOIS"
+    }
+
+    private val telefoneDoisEt: EditText by lazy {
+        EditText(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        val telefoneDoisEt = EditText(this)
+        telefoneDoisEt.hint = "Segundo telefone"
+        telefoneDoisEt.layoutParams= LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+        amb.root.addView(telefoneDoisEt)
+
         amb.abraBt.setOnClickListener{
             // Jeito Kotlin para chamar a tela do activity_another.xml
             Intent(this, AnotherActivity::class.java).also {
@@ -25,6 +44,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(anotherIntent)*/
         }
         Log.v(getString(R.string.app_name), "onCreate: Iniciando ciclo completo")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(VALOR_TELEFONE_UM, amb.telefoneUmEt.text.toString())
+        outState.putString(VALOR_TELEFONE_DOIS, telefoneDoisEt.text.toString())
     }
 
     override fun onStart() {
