01004a6309774ebcfe77f4851a7a4d6a0d137506
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2024-09-09 20:18:28 -0300

    part3

--- app/src/main/java/br/edu/scl/ifsp/ads/pdm/cdv/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/pdm/cdv/MainActivity.kt
@@ -50,6 +50,26 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         outState.putString(VALOR_TELEFONE_UM, amb.telefoneUmEt.text.toString())
         outState.putString(VALOR_TELEFONE_DOIS, telefoneDoisEt.text.toString())
+        Log.v(getString(R.string.app_name), "onSaveInstanceState: Salvando dados de instancia")
+    }
+
+    // metodo chamado quando a tela for recriado
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+//        savedInstanceState.apply {
+//            val valorTelefoen
+//        }
+
+        savedInstanceState.apply {
+            getString(VALOR_TELEFONE_UM)?.let {  // só é executado se esse metodo nao retornar nulo
+                amb.telefoneUmEt.setText(it)
+            }
+            getString(VALOR_TELEFONE_DOIS, "000000000").let {
+                telefoneDoisEt.setText(it)
+            }
+        }
+        Log.v(getString(R.string.app_name), "onRestoreInstanceState: Restaurando os15 dados de instancia")
     }
 
     override fun onStart() {
